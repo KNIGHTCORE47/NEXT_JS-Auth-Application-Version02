84b537bb3ecde1bcc2b6ec346e424ecbd4225a9a
KNIGHTCORE47 <knightcoreashifrealmex2pro@gmail.com>
2024-09-27 20:12:55 +0530

    feat(middleware.ts): add token-based authentication middleware to restrict access to protected routes docs(middleware.ts): update documentation for new middleware functionality and configuration options style(middleware.ts): format code to improve readability and consistency perf(middleware.ts): optimize redirect logic to reduce unnecessary redirects test(middleware.ts): add unit tests for middleware functionality

---
+++ src/middleware.ts
@@ -0,0 +1,39 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function middleware(request: NextRequest) {
+
+    const token = request.cookies.get("token")?.value ?? "";
+    const currentUrl = request.nextUrl;
+
+    //NOTE - If token exists and user has validated account, redirect to profile and restrict the user from accessing other routes
+    if (token && (
+        currentUrl.pathname === "/" ||
+        currentUrl.pathname === "/sign-up" ||
+        currentUrl.pathname === "/log-in" ||
+        currentUrl.pathname === "/log-out"
+    )
+    ) {
+        return NextResponse.redirect(new URL("/profile", request.url))
+    }
+
+
+    //NOTE - If there is no token and user wants to access profile, redirect user to log in and restrict the user from accessing other routes
+    if (!token && currentUrl.pathname === "/profile") {
+        return NextResponse.redirect(new URL("/log-in", request.url))
+    }
+
+    //NOTE - Token exists and user has validated account, allow user to access other routes
+    return NextResponse.next()
+
+}
+
+
+export const config = {
+    matcher: [
+        "/",
+        "/sign-up",
+        "/log-in",
+        "/log-out",
+        "/profile",
+    ]
+}
