f7e4c19498dec05c78f86619ccec09359e903a34
KNIGHTCORE47 <knightcoreashifrealmex2pro@gmail.com>
2024-09-27 23:35:51 +0530

    docs(profile/page.tsx): update layout to center content vertically and horizontally for better readability feat(profile/page.tsx): add span element around profile id for styling purposes

--- src/app/(app)/profile/[id]/page.tsx
+++ src/app/(app)/profile/[id]/page.tsx
@@ -7,11 +7,11 @@ import React from 'react'
 function profilePageUserParams() {
     const { id } = useParams<{ id: string }>();
     return (
-        <div className='p-4'>
+        <div className='min-h-screen grid place-items-center bg-black text-white'>
             <h1
                 className='text-center text-base sm:text-lg md:text-2xl'
             >
-                {id}
+                Here is the profile: <span className='text-gray-500 px-2 py-1 border-2 border-gray-500 rounded-lg'>{id}</span>
             </h1>
         </div>
     )
